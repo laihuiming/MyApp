5a9b73220d0c4ffc3e6c28f4b90c3ecc9bc30b8d
laihuiming <2505131649@qq.com>
2021-07-26 11:03:02 +0800

    Tablayout+ViewPager+Fragment完成

--- app/src/main/java/com/example/myapplication2/ViewPager/ViewPagerActivity.java
+++ app/src/main/java/com/example/myapplication2/ViewPager/ViewPagerActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication2.ViewPager;
 
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -25,18 +28,11 @@ public class ViewPagerActivity extends AppCompatActivity {
     TabLayout tabLayout;
     @BindView(R.id.viewpager)
     ViewPager viewpager;
-    @BindView(R.id.tv_fvt_one)
-    TextView tvFvtOne;
-    @BindView(R.id.tv_fvt_two)
-    TextView tvFvtTwo;
-    @BindView(R.id.tv_fvt_three)
-    TextView tvFvtThree;
-    @BindView(R.id.tv_fvt_four)
-    TextView tvFvtFour;
+
     private ViewPagerAdapter adapter;
 
     //设置界面文件和文字一一对应
-    private Fragment[] mfragments = {new TabOneFragment(), new TabTwoFragment(), new TabThreeFragment(), new TabThreeFragment()};
+    private Fragment[] mfragments = {new TabOneFragment(), new TabTwoFragment(), new TabThreeFragment(), new TabFourFragment()};
     String[] mtitles = {"茶", "啤酒", "果汁", "咖啡"};
 //    private List<Fragment> mFragmentList = new ArrayList<>();
     //未选中图片
@@ -57,17 +53,70 @@ public class ViewPagerActivity extends AppCompatActivity {
     }
 
     private void initTabLayout() {
-        ItemSelect();
-        ItemNoSelect();
+        //初始化
+        tabLayout.getTabAt(ItemWhat);
+        for (int i = 1; i < tabLayout.getTabCount();i++){
+            View view = LayoutInflater.from(ViewPagerActivity.this).inflate(R.layout.item_view1,null);
+//            LinearLayout linearLayout = view.findViewById(R.id.ll_tab1);
+            TextView tabText = view.findViewById(R.id.tv_tab1);
+            ImageView imageView = view.findViewById(R.id.iv_tab1);
+
+            tabText.setText(mtitles[i]);
+            imageView.setImageResource(unimg[i]);
+
+            if (i == ItemWhat){
+                imageView.setImageResource(img[i]);
+            }
+
+            //设置样式
+            tabLayout.getTabAt(i).setCustomView(view);
+        }
+        //监听
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                //选中时进入，改变样式
+                ItemSelect(tab);
+                //onTabselected方法里面调用了viewPager的setCurrentItem 所以要想自定义OnTabSelectedListener，也加上mViewPager.setCurrentItem(tab.getPosition())就可以了
+                viewpager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                //未选中状态
+                ItemNoSelect(tab);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                //重新选中
+            }
+        });
     }
     //tabLayout未选中状态
     private void ItemNoSelect(TabLayout.Tab tab) {
-
+        View view = tab.getCustomView();
+        TextView tabText = view.findViewById(R.id.tv_tab1);
+        ImageView imageView = view.findViewById(R.id.iv_tab1);
+        String stitle = tabText.getText().toString();
+        for (int i = 0; i < mtitles.length;i++){
+            if (mtitles[i].equals(stitle)){
+                imageView.setImageResource(unimg[i]);
+            }
+        }
     }
 
     //tabLayout选中状态
     private void ItemSelect(TabLayout.Tab tab) {
         View view = tab.getCustomView();
+        TextView tabText = view.findViewById(R.id.tv_tab1);
+        ImageView imageView = view.findViewById(R.id.iv_tab1);
+        String stitle = tabText.getText().toString();
+        for (int i = 0; i < mtitles.length;i++){
+            if (mtitles[i].equals(stitle)){
+                imageView.setImageResource(img[i]);
+            }
+        }
 
     }
 
